c82b246ea16c70087b0580a688e0706b69fc4565
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-07-27 16:00:10 +0530

    added compare to methods

--- CompareToMethods.java
+++ CompareToMethods.java
@@ -1,24 +1,27 @@
 package com.bridgelabz.java.linecomparison;
 
-public class GreaterOrLess {
+public class CompareToMethods {
 	int x1, x2, y1, y2;
 	int a1, a2, b1, b2;
 
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to Line Comparision");
-		GreaterOrLess method = new GreaterOrLess();
-		method.Length();
+		CompareToMethods Compare = new CompareToMethods();
+
+		Compare.Lengthc();
+
 	}
 
-	public void Length() {
+	public void Lengthc() {
 		Double distance_a, distance_b;
-		x1 = 1;  y1 = 2;  x2 = 3;  y2 = 7;
-	    a1 = 2;  b1 = 3;  a2 = 5;  b2 = 6;
+		x1 = 1; y1 = 10; x2 = 5; y2 = 6;
+		a1 = 1; b1 = 6; a2 = 5; b2 = 6;
 		distance_a = Math.sqrt(Math.pow((x2 - x1), 2) + (Math.pow((y2 - y1), 2)));
-		System.out.println("Calculated len1: " + distance_a);
+		System.out.println("Calculated len1" + distance_a);
 		distance_b = Math.sqrt(Math.pow((a2 - a1), 2) + (Math.pow((b2 - b1), 2)));
-		System.out.println("Calculated len2: " + distance_b);
-		if (distance_a.compareTo(distance_b) == 0) {
+		System.out.println("Calculated len2:" + distance_b);
+		if (distance_a.equals(distance_b)) {
 			System.out.println("Line1 is  Equal to Line2");
 
 		} else if (distance_a.compareTo(distance_b) == -1) {
@@ -27,5 +30,4 @@ public class GreaterOrLess {
 			System.out.println("Line1 Greater than Line2");
 		}
 	}
-
 }
